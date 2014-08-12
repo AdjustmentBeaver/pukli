e295d3bd21e5235e11226874f6fa46a7e11ded5c
Dömötör Szilárd <szilard@adjustmentbeaver.com>
2014-08-12 19:00:47 +0200

    added comments

--- Pukli/src/Game.h
+++ Pukli/src/Game.h
@@ -6,37 +6,42 @@
 #include "Game_object.h"
 #include "Game_state_machine.h"
 
-
 class Game {
 public:
-
+	// Singleton
 	static Game* instance() {
 		if (s_instance == 0)
 			s_instance = new Game();
 		return s_instance;
 	}
 
-	bool init(const char*, int, int, int, int, bool);
+	// Init - window parameters
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
 	
+	// Game loop functions
 	void update();
 	void render();
 	void handleEvents();
 	void clean();
 	void quit();
-
 	bool running() const { return m_running; }
 
+	// SDL functions
 	SDL_Renderer* get_renderer() const { return m_renderer; }
 
 private:
+	// Singleton
 	Game() {}
 	static Game* s_instance;
 
+	// SDL data
 	SDL_Window* m_window;
 	SDL_Renderer* m_renderer;
 
+	// State machine
 	Game_state_machine* m_game_state_machine;
 
+	// Game loop data
 	bool m_running;
 };
 
