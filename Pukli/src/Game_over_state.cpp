8d46783e95d92380590b500fb4c9e9f6608f24b3
Dömötör Szilárd <szilard@adjustmentbeaver.com>
2014-08-15 15:41:24 +0200

    added game over state

---
+++ Pukli/src/Game_over_state.cpp
@@ -0,0 +1,61 @@
+#include "Game_over_state.h"
+#include "Menu_state.h"
+#include "Menu_button.h"
+#include "Play_state.h"
+#include "Game.h"
+#include "Texture_manager.h"
+#include "Animated_graphic.h"
+
+const std::string Game_over_state::s_game_over_id = "GAME_OVER";
+
+void Game_over_state::game_over_to_main() {
+	The_Game->get_state_machine()->clear();
+	The_Game->get_state_machine()->change_state(new Menu_state());
+}
+
+void Game_over_state::game_over_to_play() {
+	The_Game->get_state_machine()->clear();
+	The_Game->get_state_machine()->change_state(new Play_state());
+}
+
+bool Game_over_state::on_enter() {
+	if (!The_Texture_manager->load("../assets/btn_start.png", "btn_restart", The_Game->get_renderer()))
+		return false;
+	if (!The_Texture_manager->load("../assets/btn_exit.png", "btn_menu", The_Game->get_renderer()))
+		return false;
+
+
+	Loader_params * tmp = new Loader_params(100, 100, 200, 200, "btn_restart");
+	m_game_objects.push_back(new Menu_button(tmp, game_over_to_play));
+	delete tmp;
+	tmp = new Loader_params(100, 500, 150, 50, "btn_menu");
+	m_game_objects.push_back(new Menu_button(tmp, game_over_to_main));
+	delete tmp;
+
+
+	return true;
+}
+
+bool Game_over_state::on_exit() {
+
+	for (std::vector<Game_object*>::size_type i = 0; i < m_game_objects.size(); i++) {
+		m_game_objects[i]->clean();
+		delete m_game_objects[i];
+	}
+	m_game_objects.clear();
+	The_Texture_manager->clear_from_texture_map("btn_restart");
+	The_Texture_manager->clear_from_texture_map("btn_menu");
+
+
+	return true;
+}
+void Game_over_state::update() {
+	for (std::vector<Game_object*>::size_type i = 0; The_Game->get_state_machine()->get_current_state() == this && i<m_game_objects.size(); i++) {
+		m_game_objects[i]->update();
+	}
+}
+void Game_over_state::render() {
+	for (std::vector<Game_object*>::size_type i = 0; The_Game->get_state_machine()->get_current_state() == this && i < m_game_objects.size(); i++) {
+		m_game_objects[i]->draw();
+	}
+}
---
+++ Pukli/src/Game_over_state.h
@@ -0,0 +1,25 @@
+#ifndef __GAME_OVER_STATE_H__
+#define __GAME_OVER_STATE_H__
+
+#include <vector>
+#include "Game_state.h"
+class Game_object;
+
+class Game_over_state : public Game_state {
+public:
+	void update();
+	void render();
+
+	bool on_enter();
+	bool on_exit();
+
+	const std::string get_state_id() const { return s_game_over_id; };
+private:
+	static const std::string s_game_over_id;
+	std::vector<Game_object*> m_game_objects;
+
+	static void game_over_to_main();
+	static void game_over_to_play();
+};
+
+#endif
