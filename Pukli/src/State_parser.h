6244c7fbe5d6899da2af38940802dde6600ec5fc
András Földvári <fandras95@gmail.com>
2014-08-16 19:50:50 +0200

    Fix State_parser

--- Pukli/src/State_parser.h
+++ Pukli/src/State_parser.h
@@ -4,6 +4,7 @@
 #include "rapidjson\document.h"
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace rapidjson;
 
@@ -14,9 +15,11 @@ class State_parser
 public:
 	bool parse_state(const char* file_name, std::string state_id, std::vector<Game_object*> *p_objects);
 private:
-	void parse_objects(Value& p_state_root, std::vector<Game_object*> *p_objects);
+	void parse_objects(const Value& p_state_root, std::vector<Game_object*> *p_objects);
 	
-	void parse_textures(Value& p_state_root, std::vector<std::string> *p_texture_ids);
+	void parse_textures(const Value& p_state_root, std::vector<std::string> *p_texture_ids);
+
+	std::string get_file_contents(const char *filename);
 };
 
 #endif
