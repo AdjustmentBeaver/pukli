650e6c70ecdc2e6572d5e8f9b9bd705c02867f08
Dömötör Szilárd <szilard@adjustmentbeaver.com>
2014-08-16 19:53:29 +0200

    parameter names; removed comment

--- Pukli/src/Player.cpp
+++ Pukli/src/Player.cpp
@@ -15,9 +15,9 @@ void Player::update() {
 	SDL_Game_object::update();
 }
 
-void Player::load(const Loader_params *pParams) 
+void Player::load(const Loader_params *params) 
 {
-	SDL_Game_object::load(pParams); 
+	SDL_Game_object::load(params);
 }
 
 void Player::clean() {}
@@ -28,7 +28,6 @@ void Player::handleInput() {
 		m_acceleration = Vec2D(lx/10.0,ly/10.0);
 	if (The_Input_handler->get_joy_button_state(0,0)) m_velocity = Vec2D();
 	}
-	
 	if (The_Input_handler->is_key_down(SDL_SCANCODE_UP))
 		m_velocity.set_y(-2);
 
@@ -40,6 +39,4 @@ void Player::handleInput() {
 
 	if (The_Input_handler->is_key_down(SDL_SCANCODE_RIGHT))
 		m_velocity.set_x(2);
-
-	//m_velocity = (*The_Input_handler->get_mouse_position() - m_position) / 100;
 }
